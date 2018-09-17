b9653472d45e01655a002e698521fa4252469103
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-09-17 16:32:02 -0400

    Update profile.py
    refactored with list

--- profile.py
+++ profile.py
@@ -17,38 +17,43 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 #Establish number of nodes wanted
-nodes=4
-i=1
+numNodes=4
+nodes = [0] * numNodes
+#Memeber for links
+members = [0] * numNodes
 
-#Establish local network
-link = request.Link("Link")
-
-while i<=nodes:
+#add counter
+i=0
+while i<nodes:
 	# Add a VM to the request.
-	node = request.XenVM("node-"+str(i))
+	nodes[i] = request.XenVM("node-"+str(i+1))
 	#Install CentOS7-STD
-	node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+	nodes[i].disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 	
 	#add public IP for first node
 	if i==1:
-		node.routable_control_ip = True
+		nodes[i].routable_control_ip = True
 	
 	#add interface
-	iface1 = node.addInterface("if"+str(i))
+	iface = nodes[i].addInterface("if"+str(i+1))
 	#add interface eth1, will be same for all nodes
-	iface1.component_id = "eth1"
+	iface.component_id = "eth1"
 	#set IP per VM
-	iface1.addAddress(rspec.IPv4Address("192.168.1."+str(i), "255.255.255.0")
-	
-	#Link nodes together
-	link.addInterface(iface1)
-	
+	iface.addAddress(rspec.IPv4Address("192.168.1."+str(i+1), "255.255.255.0")
+
+    #add node to members
+    members[i] = nodes[i]
+                  
 	# Install and execute a script that is contained in the repository.
 	node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
 	
 	#increment i for next node
 	i+=1
 
+#Establish local network
+link = request.Link("Link")
+#Link nodes together
+link.addLink(members)
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
